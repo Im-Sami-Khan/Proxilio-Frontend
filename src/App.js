3e6e8fd6c7f6f67da3f8fbd7d48b5dc149cd9a53
Sami Khan <samikhan.dev@gmail.com>
2023-06-08 12:43:24 -0400

    Merged routes in ApolloProvider, added ApolloClient and InMemoryCache in it

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Project from './pages/Project';
-import NotFound from './pages/NotFound';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+import Home from "./pages/Home";
+import Project from "./pages/Project";
+import NotFound from "./pages/NotFound";
+
+const URI = "http://localhost:3000/graphql";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(_existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(_existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: URI,
+  cache,
+});
 
 const App = () => {
   return (
     <>
-      <Router>
-        <div className='container'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/projects/:id' element={<Project />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </div>
-      </Router>
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects/:id" element={<Project />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
+      </ApolloProvider>
     </>
   );
-}
+};
 export default App;
